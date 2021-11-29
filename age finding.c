728f310d20db24d4dcfbb660a5a8371e5b291745
Alish Rani <94797867+AlishRani@users.noreply.github.com>
2021-11-29 18:26:08 +0530

    Add files via upload

---
+++ age finding.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   int age;
+   printf("enetr the age\n");
+   scanf("%d",&age);
+   if(age>90){
+      printf("age is above 90");
+  }
+  else{
+      printf("age is below 90");
+  }
+  return 0;
+}
